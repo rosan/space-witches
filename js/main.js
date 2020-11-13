de35b3df869a3ef894e3253104488063608880a2
Rosa Nussbaum <rosa@rosanussbaum.com>
2020-11-13 17:57:56 -0500

    snap controls to object, make stars round

--- js/main.js
+++ js/main.js
@@ -9,10 +9,11 @@ import {GLTFLoader} from 'https://unpkg.com/three@0.121.1//examples/jsm/loaders/
 
 import {OrbitControls} from 'https://unpkg.com/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
-let scene, camera, renderer, cube1,  mouse, mouseDown, mouseMove, mouseClick, mouseOver, mouseSound, mouseSpeech, controls ;
+let scene, camera, renderer, mouse, cube1, mouseDown, mouseMove, mouseClick, mouseOver, mouseSound, mouseSpeech, controls, starField3, starFieldMaterial3, destination ;
 
 let zombieMouse = {
     gltfScene: null,
+    boundingBox: null,
     mesh: null,
     animations: null,
     materialMap: null,
@@ -42,6 +43,80 @@ let text = document.createTextNode("Start");
 button.appendChild(text);
 
 button.addEventListener('click', runProgram, false);
+
+//changing orbital controls target
+let alpha=1;
+function changeTarget(){
+    alpha = alpha + 0.1*delta;
+
+    if(alpha > 0.15){
+        alpha = 1;
+    }
+    controls.target.lerp(destination, alpha);
+    console.log(alpha);
+}
+
+//Bounding boxes
+function makeZombiMouseBoundingBox (){
+    const geometry = new THREE.BoxGeometry(6,17,7);
+
+    // const texture = new THREE.TextureLoader().load('texture/brick-texture.jpg')
+    // const material = new THREE.MeshBasicMaterial();
+    cube1 = new THREE.Mesh(geometry);
+    cube1.visible = false;
+    cube1.position.x = 0;
+    cube1.position.y = 7;
+    cube1.position.z = -3;
+    cube1.name = 'zombie-mouse-bounding-box';
+    cube1.parent = zombieMouse.gltfScene;
+    console.log(zombieMouse.gltfScene);
+
+
+    // scene.add(cube1)
+}
+
+function loadZombieMouse(){
+    // Loading in zombie mouse model
+    const loader = new GLTFLoader();
+    loader.load('models/full-mouse-2-animation.gltf', function (gltf) {
+        zombieMouse.gltfScene = gltf.scene;
+        zombieMouse.gltfScene.position.x = 20;
+        zombieMouse.gltfScene.position.y = 5;
+        zombieMouse.gltfScene.position.z = 3;
+
+
+        zombieMouse.animations = gltf.animations;
+
+        scene.add(gltf.scene);
+        console.log(zombieMouse.gltfScene.children[0]);
+        console.log(zombieMouse.animations);
+
+
+        //Extracting material: make this more elegant: breadth first search
+        zombieMouse.mesh = zombieMouse.gltfScene.children[0].children.filter(function(m){return m.name=='prelim-mouse-blender-3'});
+        zombieMouse.materialMap = zombieMouse.mesh[0].material.map;
+
+        makeZombiMouseBoundingBox ();
+        zombieMouse.gltfScene.add(cube1);
+        
+        console.log(zombieMouse.gltfScene)
+
+
+        //Animation
+        zombieMouse.mixer = new THREE.AnimationMixer( zombieMouse.gltfScene.children[0] );
+        const clips = zombieMouse.animations;
+
+        const action = zombieMouse.mixer.clipAction(clips[0]);
+        action.play();
+
+    }, undefined, function ( error ) {
+
+        console.error( error );
+
+    } );
+
+
+}
 
 function runProgram (event) {
     init();
@@ -129,7 +204,12 @@ function init(){
         
     
     // Controls
-    controls = new OrbitControls(camera, renderer.domElement );   
+    controls = new OrbitControls(camera, renderer.domElement );
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.00008;
+    destination = controls.target;
+
+      
     
     //Light
     const light = new THREE.PointLight(0xFFFFFF, 1, 500);
@@ -137,56 +217,80 @@ function init(){
     scene.add(light);
 
     camera.position.z = 40;
-    controls.update();
 
+    //sky
+    const skySphere = new THREE.SphereGeometry(500,60,40);
+    const equiMaterial = new THREE.MeshBasicMaterial( {
+       map: new THREE.TextureLoader().load( 'texture/Nasa-sky-map.jpg' ),   side: THREE.BackSide
+    });
 
-    // Geometry created
-    const geometry = new THREE.BoxGeometry(6,17,7);
-    // const texture = new THREE.TextureLoader().load('texture/brick-texture.jpg')
-    // const material = new THREE.MeshLambertMaterial({map: texture});
-    cube1 = new THREE.Mesh(geometry);
-    cube1.position.x = 0;
-    cube1.position.y = 7;
-    cube1.position.z = -3;
+    const skyMesh = new THREE.Mesh(skySphere, equiMaterial);
+    // scene.add(skyMesh);
 
-    cube1.name = 'zombie-mouse-bounding-box'
-    // scene.add(cube);
+    //make starfield
+    const starDisk = new THREE.TextureLoader().load( 'texture/disc.png' );
 
-    console.log(cube1);
-
-    // scene.add(cube);
-
-    // Loading in zombie mouse model
-    const loader = new GLTFLoader();
-    loader.load('models/full-mouse-1-animation-embed-4.gltf', function (gltf) {
-        zombieMouse.gltfScene = gltf.scene;
-        zombieMouse.animations = gltf.animations;
-
-        scene.add(gltf.scene);
-        console.log(zombieMouse.gltfScene.children[0]);
-        console.log(zombieMouse.animations);
-
-
-        //make this more elegant: breadth first search
-        zombieMouse.mesh = zombieMouse.gltfScene.children[0].children.filter(function(m){return m.name=='prelim-mouse-blender-3'});
-        zombieMouse.materialMap = zombieMouse.mesh[0].material.map;
-
-
-        //Animation
-        zombieMouse.mixer = new THREE.AnimationMixer( zombieMouse.gltfScene.children[0] );
-        const clips = zombieMouse.animations;
-
-        const action = zombieMouse.mixer.clipAction(clips[0]);
-        action.play();
-
-    }, undefined, function ( error ) {
-
-        console.error( error );
-
-    } );
-
-
+    const starGeometry = new THREE.Geometry();
+    for (let i = 0; i < 2000; i++) {
+        const vertex = new THREE.Vector3();
+        vertex.x = Math.random()*1000-500;
+        vertex.y = Math.random()*1000-500;
+        vertex.z = Math.random()*1000-500;
+        starGeometry.vertices.push(vertex);
+    }
+    const starField = new THREE.Points(starGeometry, new THREE.PointsMaterial({
+        map: starDisk,
+        size: 0.1,
+        color: 0xffffff
+        })
+    );
     
+    scene.add(starField);
+    starField.position.z = 100;
+
+
+    const starGeometry2 = new THREE.Geometry();
+    for (let i = 0; i < 1000; i++) {
+        const vertex = new THREE.Vector3();
+        vertex.x = Math.random()*1000-800;
+        vertex.y = Math.random()*1000-800;
+        vertex.z = Math.random()*1000-800;
+        starGeometry2.vertices.push(vertex);
+    }
+    const starField2 = new THREE.Points(starGeometry2, new THREE.PointsMaterial({
+        map: starDisk,
+        size: 0.7,
+        color: 0xa8e6ff
+        })
+    );
+    
+    scene.add(starField2);
+    starField2.position.z = 150;
+
+
+    const starGeometry3 = new THREE.Geometry();
+    for (let i = 0; i < 1000; i++) {
+        const vertex = new THREE.Vector3();
+        vertex.x = Math.random()*1000-500;
+        vertex.y = Math.random()*1000-500;
+        vertex.z = Math.random()*1000-200;
+        starGeometry3.vertices.push(vertex);
+    }
+    starFieldMaterial3 = new THREE.PointsMaterial({
+        map: starDisk,
+        size: 0.4,
+        color: 0xffc47d
+        })
+    starField3 = new THREE.Points(starGeometry3, starFieldMaterial3);
+
+   
+    
+    scene.add(starField3);
+    starField3.position.z = 130;
+
+
+
+    loadZombieMouse()
 
 
     zombieMouse.hoverTexture = new THREE.TextureLoader().load('texture/brick-texture.jpg');
@@ -272,7 +376,6 @@ function init(){
 
 
 
-
 const animate = function animate () { 
     requestAnimationFrame(animate);
     if(!zombieMouse.gltfScene){return;}
@@ -287,12 +390,8 @@ const animate = function animate () {
     zombieMouse.gltfScene.add(mouseSound);
     zombieMouse.gltfScene.add(mouseSpeech);
 
-    cube1.rotation.x += 0.3 * delta;
-    cube1.rotation.y += 0.3 * delta;
-
-
-
-
+    
+    // starField3.material.color.set(0x852121);
 
 
     // update the picking ray with the camera and mouse position
@@ -307,10 +406,9 @@ const animate = function animate () {
 
 
 
-
     if(intersects.length > 0 && intersects[0].object.name=='zombie-mouse-bounding-box'){
+        // console.log(controls.target);
 
-        
         zombieMouse.hoverTexture.wrapS = THREE.RepeatWrapping;
         zombieMouse.hoverTexture.wrapT = THREE.RepeatWrapping;
         zombieMouse.hoverTexture.flipY = false;
@@ -321,18 +419,25 @@ const animate = function animate () {
     }
 
 
-    if (mouseClick && intersects.length > 0 && intersects[0].object.name=='zombie-mouse-bounding-box'){
+    if (mouseClick && intersects.length > 0 && intersects[0].object.name== 'zombie-mouse-bounding-box'){
         
         console.log('Intersection click', intersects[0].object.name)
         mouseSpeech.play();
+
+        // controls.target = zombieMouse.gltfScene.position;
+        alpha = 0;
+        destination = zombieMouse.gltfScene.position;
+
 
         //silent audio and captions play
         const audio = document.getElementById("audio-1");
         audio.play();    
     }
     
-        
- 
+    changeTarget();
+
+    controls.update();
+
     
     renderer.render(scene, camera);
 
